4b70f5006775d0f30bacb0625816318fc335a289
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-08-26 14:18:05 +0200

    Insert 3 countries test

---
+++ database/seed.sql
@@ -0,0 +1,7 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO countries (country_name, continent, flagurl)
+VALUES
+('Japan', 'Asia', 'https://flagcdn.com/w320/jp.png'),
+('Italy', 'Europe', 'https://flagcdn.com/w320/it.png'),
+('Mexico', 'North America', 'https://flagcdn.com/w320/mx.png');
