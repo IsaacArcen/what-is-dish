d2d72bb0cacae31d31c2669c46607592d435d26c
Robert Österberg <rosterberg93@gmail.com>
2026-09-01 13:56:53 +0200

    Added route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './HomePage'
-import Header from './Components/Header'  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import Header from "./Components/Header";
+import QuizSettings from "./QuizSettings";
 
 function App() {
   return (
@@ -8,9 +9,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<QuizSettings />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
