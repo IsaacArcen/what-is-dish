a82f727f35418bf13d79b13968bb78050c3bc51d
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-09-14 16:11:36 +0200

    table recipes

--- database/schema.sql
+++ database/schema.sql
@@ -18,3 +18,11 @@ CREATE TABLE dishes (
     FOREIGN KEY (country_id) REFERENCES countries(country_id),
     UNIQUE (country_id, dish_name)
 );
+
+CREATE TABLE recipes (
+    recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    country_id INTEGER NOT NULL UNIQUE,
+    ingredients TEXT NOT NULL,
+    steps TEXT NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(country_id),
+);
