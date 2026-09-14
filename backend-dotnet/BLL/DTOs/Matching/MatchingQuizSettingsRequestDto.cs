6f1d1cbcff6c9c6bd5025c4853e858ceb47a42c4
AngelosLekkas <lekkas.angelos@gmail.com>
2026-09-14 02:36:50 +0200

    requestDto för matchingQuiz med numberOfCountries

---
+++ backend-dotnet/BLL/DTOs/Matching/MatchingQuizSettingsRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WhatIsDish.Api.BLL.DTOs.Matching;
+
+public class MatchingQuizSettingsRequestDto
+{
+    //antal matchningar som quizet ska innehålla (5,10,20)
+    public int NumberOfCountries { get; set; }
+
+    //valda regioner
+    public List<string> Continents { get; set; } = new();
+}
